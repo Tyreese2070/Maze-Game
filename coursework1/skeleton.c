78519da21e174b7e55fbf8e8c6ff7537c58f3814
Tyreese2070 <146450901+Tyreese2070@users.noreply.github.com>
2024-02-14 10:18:32 +0000

    Started skeleton code

---
+++ coursework1/skeleton.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Maze game
+struct Maze{
+    int width;
+    int height;
+    char[50] filepath;
+} maze;
+
+// Move the user
+void moveCharacter(char userInput, Maze*){
+}
+
+// Load maze file
+void getMaze(){
+}
+
+// View an image of the map
+void viewMap(){
+}
+
+// Main with command line args
+int main(char argc, char *argv[])
+{
+    // Load the maze file
+
+    // Validate maze file
+
+    // Check for user input
+
+    // Check for game events
+
+    // Quit the game
+}
