25ed20649c1bcc7df76100b8e236810c4086ac48
Tyreese2070 <146450901+Tyreese2070@users.noreply.github.com>
2024-04-29 08:38:28 +0000

    tested c++

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    float num1 = 1.12345678765432123456723515;
+    double num2 = 5.0987574757635642562532543;
+
+    printf("%f \n", num1);
+    printf("%f \n", num2);
+
+    return 0;
+}
